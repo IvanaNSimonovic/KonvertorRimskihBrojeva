1c26aecbdac20c2637525f6c518b7d104f39ac11
IvanaNSimonovic <50739781+IvanaNSimonovic@users.noreply.github.com>
2019-11-11 14:46:12 +0100

    Update KonvertorRimskihBrojeva.java

--- KonvertorRimskihBrojeva.java
+++ KonvertorRimskihBrojeva.java
@@ -1,61 +1,38 @@
 package konvertorRimskih;
+import java.util.HashMap;
 
-import java.util.Scanner;
-
-public class KonvertorRimskihBrojeva {
+public class RomanConverter {
+	private static HashMap<Character, Integer> map = new HashMap<>();
+	static {
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+	}
 	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		String rim=s.next();
-		int zb=0;
-		for(int i=0; i<rim.length(); i++) {
-			char trenutni=rim.charAt(i);
-			switch(trenutni) {
-			case 'I':if(i+1<=rim.length() && i+2<=rim.length() && rim.charAt(i+1)=='I' && rim.charAt(i+2)=='I') {
-				zb+=3;
-				i+=2;
-				break;
-			} else if(i+1<=rim.length() && rim.charAt(i+1)=='I') {
-				zb+=2;
-				i++;
-				break;
-			}else if(i+1<=rim.length() && rim.charAt(i+1)=='V') {
-				zb+=4;
-				i++;
-				break;
-			}else if(i+1<=rim.length() && rim.charAt(i+1)=='X') {
-				zb+=9;
-				i++;
-				break;
-			}
-			case 'V':zb+=5;break;
-			case 'X':if(i+1<=rim.length() && rim.charAt(i+1)=='L') {
-				zb+=40;
-				i++;
-				break;
-			}else if(i+1<=rim.length() && rim.charAt(i+1)=='C') {
-				zb+=90;
-				i++;
-				break;
-			} else 
-				zb+=10;break;
-			case 'L':zb+=50;break;
-			case 'C':if(i+1<=rim.length() && rim.charAt(i+1)=='D') {
-				zb+=400;
-				i++;
-				break;
-			}else if(i+1<=rim.length() && rim.charAt(i+1)=='M') {
-				zb+=900;
-				i++;
-				break;
-			} else 
-				zb+=100;break;
-			case 'D':zb+=500;break;
-			case 'M':zb+=1000;break;
-				
+	public static int convert(String roman) {		
+		int num = 0;
+		int acc = 0;
+		
+		for(int i=0; i<roman.length(); i++) {
+			int val = map.get(roman.charAt(i));
+			if(acc!=0 && val > acc) {
+				num += val - acc;
+				acc = 0;
+			} else {
+				num += acc;
+				acc = val;
 			}
 		}
-		System.out.println(zb);
-
+		
+		return num + acc;
 	}
-}
+	
+	public static void main(String[] args) {
+		String[] tests = {"MCMLXXIII", "IV", "IX", "LVIII", "MCMLXXXIV", "MCMXCIV"};
+		for(String test: tests)
+			System.out.printf("%s => %d\n", test, convert(test));
+	}
